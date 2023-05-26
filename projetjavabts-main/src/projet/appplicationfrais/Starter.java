d44ae9a3344eeb2c7e5686c385b37fc61af849d9
Ewen29000 <134706061+Ewen29000@users.noreply.github.com>
2023-05-26 16:11:47 +0200

    Add files via upload

---
+++ projetjavabts-main/src/projet/appplicationfrais/Starter.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet.appplicationfrais;
+
+import java.sql.*;
+
+/**
+ *
+ * @author tyvinec
+ */
+public class Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        MainWindow f1 = new MainWindow();
+//        f1.setVisible(true);
+        authentification a1 = new authentification();
+        a1.setVisible(true);
+    }
+    
+}
